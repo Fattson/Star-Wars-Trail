f0ea0e6f689f72ac977cf0143386db17b182a247
Victor Hugo <victorhlvsilva@gmail.com>
2016-05-09 16:15:36 -0300

    Cenário 'ok'

--- TestePygame.py
+++ TestePygame.py
@@ -1,70 +1,76 @@
-from pygame import *
-#import loopJogo.py as lj
-import SwTrail.py as sw
+import pygame
+from pygame.locals import *
+from random import randint
 
-init()
+pygame.init() # Iniciar pygame
 
-screen = display.set_mode((956,560), 0, 32)
+screen = pygame.display.set_mode((956,560), 0, 32) # Definir tela e suas dimensões
+
+pygame.display.set_caption("Testando, 1.. 2.. 3..") # Título da Sceen
+
 #sur = pygame.Surface((30,30), flags=0, depth=)
 #circulo = pygame.draw.circle(screen, (0,0), (100,100), 5, width=0)
 
-x,y = (100,100)
-#imagem = pygame.image.load("oi.png").convert_alpha()
-bg = image.load("bg.png").convert()
-fonte = font.Font(None, 30)
+imagem = pygame.image.load("oi.png").convert() # Carregar a imagem, .convert_alpha() serve para a imagem ficar em cima do Plano de Fundo
+bg = pygame.image.load("bg.png").convert()
+bg2 = pygame.image.load("bg.png").convert()  # Carregar a imagem, .convert() serve para a imagem ser o Plano de Fundo
+xis = pygame.image.load("Xis.jpg").convert()
+barra = pygame.image.load("barra_500x50.png").convert()
+clock = pygame.time.Clock() # Tempo
+image_position = [-500,0] # Colocar fora do while --'
+circle_position = [100,100]
+largura = 0
+ya =  0
+bg_x = 0
+bg2_x = 956
+while True: # Tem que ter haha
+	
+	for event in pygame.event.get(): # Todos os eventos do pygame
+		if event.type == pygame.QUIT: # QUIT é para fechar a tela
+			exit()
 
-screen.fill((0,0,0))
-clock = time.Clock()
-
-#screen.blit(bg, (0,0))
-
-#meuJogador = sw.Jogo()
-
-cid = fonte.render("CIDADE! O que deseja fazer?", 1, (255,255,255))
-cid_menu0 = fonte.render("0 - Continuar a viagem", 1, (255,255,255))
-cid_menu1 = fonte.render("1 - Mercado", 1, (255,255,255))
-cid_menu2 = fonte.render("2 - SUS", 1, (255,255,255))
-cid_menu3 = fonte.render("3 - Hospital Particular", 1, (255,255,255))
-cid_menu4 = fonte.render("4 - Conserto do carro", 1, (255,255,255))
-cid_menu5 = fonte.render("5 - Status", 1, (255,255,255))
-
-msg_comeco1 = fonte.render("vc e 3 amigos tao no piaui e blablabla roubaram um carro blablabla vai", 1, (255,255,255))
-msg_comeco2 = fonte.render("a viagem comeca na cidade que vcs estavam com a escola", 1, (255,255,255))
-
-screen.blit(msg_comeco1, (100,100))
-screen.blit(msg_comeco2, (100, 150))
-
-clock.tick(200)
-screen.fill((0,0,0))
-
-game_over = False
-chegou = False
-
-while game_over==False and chegou==False:
-    
-    for e in event.get():
-        if e.type == QUIT:
-            exit()
-
-    if key.get_pressed()[K_a]:
-        hw = fonte.render("Hello World!", 1, (255,255,255))
-        screen.blit(hw, (100,100))
-    if key.get_pressed()[K_e] or key.get_pressed()[K_0] or key.get_pressed()[K_KP0]:
-        screen.fill((0,0,0))
-    
-    if key.get_pressed()[K_c]: 
-        #cidade(meuJogador)
-        screen.blit(cid, (100,100))
-        screen.blit(cid_menu0, (100,150))
-        screen.blit(cid_menu1, (100,200))
-        screen.blit(cid_menu2, (100,250))
-        screen.blit(cid_menu3, (100,300))
-        screen.blit(cid_menu4, (100,350))
-        screen.blit(cid_menu5, (100,400))
-        
-    #screen.fill((0,0,0))
-    #screen.blit(bg, (0,0))
-    #screen.blit(imagem, (0,0))
-    display.update()
-    clock.tick(20)
+	speed = {'x': 2.5, 'y': 0} # Dicionário com as velocidades, não precisa ser dicionário, pode ser variávies. Mas é mais fácil
+	Cspeed= {'x': 0, 'y': 0}
+	tecla = pygame.key.get_pressed() # Registra as teclas pressionadas
+	cair = {'y': 10}
+	# Se tecla 'x' for pressionada, acontece 'y':
+	if tecla[K_d]:
+		speed['x'] = 5
+	elif tecla[K_a]:
+		speed['x'] = -5
+	if tecla[K_s]:
+		speed['y'] = 5
+	elif tecla[K_w]:
+		speed['y'] = -5
+	if tecla[K_RIGHT]:
+		Cspeed['x'] = 5
+	elif tecla[K_LEFT]:
+		Cspeed['x'] = -5
+	if tecla[K_DOWN]:
+		Cspeed['y'] = 5
+	elif tecla[K_UP]:
+		Cspeed['y'] = -5
+	rand = randint(0, 956)
+	bg_x -= 5
+	bg2_x -= 5
+	if bg_x <= -956:
+		bg_x = 956
+	if bg2_x <= -956:
+		bg2_x = 956
+	screen.blit(bg, (bg_x,0)) # Posicionar tela de fundo
+	screen.blit(bg2, (bg2_x,0))
+	image_position[0] += speed['x'] # Eixo X
+	image_position[1] += speed['y'] # Lembrar que posição no eixo Y é invertida !
+	circle_position[0] += Cspeed['x']
+	circle_position[1] += Cspeed['y']
+	screen.blit(imagem, image_position) # Plotar imagem na screen
+	screen.blit(barra, (300,400))
+	ya += cair['y'] 
+	#screen.blit(xis, (rand, ya))
+	#circulo = pygame.draw.circle(screen, (225, 0, 0), circle_position, 10)
+	if largura <= 496:
+		largura += 1
+	retângulo = pygame.draw.rect(screen, (225, 0, 0), [303, 403, largura, 46])
+	pygame.display.update() # Atualizar a tela constantemente, senão, ao mover, a imagem ela ficará arrastada
+	time = clock.tick(60) # Definir frames per second
 
