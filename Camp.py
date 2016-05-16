3c57dad50021e15dc0ceb3e36f09ced36aad9c7d
Victor Hugo <victorhlvsilva@gmail.com>
2016-05-16 18:15:16 -0300

    Não entendia como a vida funcionava

--- Camp.py
+++ Camp.py
@@ -9,17 +9,27 @@ background = pygame.image.load("campfire1.png")
 background = pygame.transform.scale(background, (956,560))
 
 carro = pygame.image.load("carrofogo.png")
-carro1 = pygame.image.load("carrofogosimples.png")
 
 carro = pygame.transform.scale(carro, (260,90))
-carro1 = pygame.transform.scale(carro1, (300,100))
+
+rect_transparente = pygame.image.load("campfire1_transparente1.png")
+
+rect_transparente= pygame.transform.scale(rect_transparente, (956,200))
+
+i = 0
+
+clock = pygame.time.Clock()
+
 while True:
 	for event in pygame.event.get():
 			if event.type == QUIT:
 				exit()
 
 	screen.blit(background, (0,0))
-	screen.blit(carro, (200,405))
-	#screen.blit(carro1, (154,400))
-
+	if i == 60:
+		pygame.draw.rect(screen, (0, 0, 0), [2,3,952,200])
+		pygame.draw.rect(screen, (255, 255, 255), [2,3,952,200], 5)
+		screen.blit(rect_transparente, (0,0))
+	if i < 60:
+		i += 1
 	pygame.display.update()
